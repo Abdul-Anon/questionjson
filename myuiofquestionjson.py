68993032ca1bb3f9abaa062a83a16e36f18155c7
Muhaimin A.  Salay Kanton <87873763+Abdul-Anon@users.noreply.github.com>
2023-01-04 18:00:10 +0000

    Update alert what I have been able to do so far

--- myuiofquestionjson.py
+++ myuiofquestionjson.py
@@ -1,8 +1,14 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
 
-root = tk.Tk()
+root = Tk()
 
+some_label = Label(root, text="Hello, world!")
+question_entry = Entry(root, width=600, font=8)
+question_entry.pack()
+some_label.pack()
+submit_button = Button(root, text="Submit", pady=2)
+submit_button.pack()
 root.geometry('600x500')
 root.title('Convert Questions to JSON')
 root.maxsize(620, 520)
